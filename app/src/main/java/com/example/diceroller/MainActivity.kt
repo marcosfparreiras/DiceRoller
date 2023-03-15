db4b0bd7d3d0354e4f9315511773260d6cd3e909
Marcos Parreiras <marcosfparreiras@gmail.com>
2023-03-15 21:06:01 +0100

    Improve performance by avoiding repeated calls for findViewById for the diceImage

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,6 +9,9 @@ import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,8 +20,9 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             rollDice()
         }
-    }
 
+        diceImage = findViewById(R.id.dice_image)
+    }
     private fun rollDice() {
         val randomInt = Random.nextInt(6) + 1
         val drawableResorce = when (randomInt) {
@@ -29,7 +33,6 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResorce)
     }
 }
